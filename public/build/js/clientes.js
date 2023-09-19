51101ab04727d4ca219c6d883233f347187d519f
yohancelis <yohandavidcelis123@gmail.com>
2023-09-19 08:30:31 -0500

    first commit

--- public/build/js/clientes.js
+++ public/build/js/clientes.js
@@ -1,3 +1,152 @@
+const url = 'https://api-72.onrender.com/api/clientes'
+
+const listarDatos = async() => {
+    let respuesta = ''
+    let body = document.getElementById('tabla')
+    //url: Es la url de la api.
+    //Al deslpegarla en el servidor colocar la api del servidor
+        fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then((resp) => resp.json()) //Obtener la respuesta y convertirla a json
+    .then(function(data) {
+        let listaClientes = data.clientes//Capturar el array devuelto por la api
+        datos = 
+        listaClientes.map(function(cliente) {//Recorrer el array
+            respuesta += `<tr><td>${cliente.usuario}</td>`+
+            `<td>${cliente.nombre}</td>`+
+            `<td>${cliente.apellido}</td>`+
+            `<td>${cliente.correo}</td>`+
+            `<td>${cliente.telefono}</td>`+
+            `<td><button class="btn btn-primary btn-sm edit-button"><i class="fas fa-edit"></i></button>
+            <button class="btn btn-danger btn-sm delete-button"><i class="fas fa-trash-alt"></i></button></td>`+
+            `</>`
+            
+            body.innerHTML = respuesta
+        })
+    })
+    //(${JSON.stringify(cliente)})
+    //"("${cliente._id}")'
+}
+
+const registrar = async () => {
+    let _usuario = document.getElementById('usuario').value;
+    let _nombre = document.getElementById('nombre').value;
+    let _apellido = document.getElementById('apellido').value;
+    let _email = document.getElementById('email').value; 
+    let _telefono = document.getElementById('telefono').value;
+  
+    if (_usuario.length > 0 && _nombre.length > 0) {
+      let cliente = {
+        usuario: _usuario,
+        nombre: _nombre,
+        apellido: _apellido,
+        correo: _email,
+        telefono: _telefono,
+      };
+  
+      fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify(cliente),
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      })
+        .then((resp) => resp.json())
+        .then((json) => {
+          if (json.msg) {
+            Swal.fire(
+              json.msg,
+              'Registrado exitosamente',
+              'success'
+            );
+
+            // After successful registration, fetch and display the updated data
+            listarDatos();
+          }
+        });
+    } else {
+      Swal.fire(
+        'El usuario y el nombre son obligatorios. Favor corregir.',
+        '',
+        'error'
+      );
+    }
+};
+
+
+  
+  const updateCliente = async (clienteId) => {
+    let _usuario = document.getElementById('usuario').value;
+    let _nombre = document.getElementById('nombre').value;
+    let _apellido = document.getElementById('apellido').value;
+    let _email = document.getElementById('email').value;
+    let _telefono = document.getElementById('telefono').value;
+  
+    let cliente = {
+      usuario: _usuario,
+      nombre: _nombre,
+      apellido: _apellido,
+      correo: _email,
+      telefono: _telefono,
+    };
+  
+    fetch(`${url}/${clienteId}`, {
+      method: 'PUT',
+      mode: 'cors',
+      body: JSON.stringify(cliente),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    })
+      .then((resp) => resp.json())
+      .then((json) => {
+        if (json.msg) {
+          Swal.fire(
+            json.msg,
+            'Actualizado exitosamente',
+            'success'
+          );
+          listarDatos(); // Refresh the list after successful update
+        }
+      });
+  };
+
+  
+  const eliminar = (clienteId) => {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: '¡No podrás recuperar esta información!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminarlo',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`${url}/${clienteId}`, {
+          method: 'DELETE',
+          mode: 'cors',
+        })
+          .then((resp) => resp.json())
+          .then((json) => {
+            if (json.msg) {
+              Swal.fire('Eliminado', 'El cliente ha sido eliminado.', 'success');
+              listarDatos(); // Refresh the list after successful deletion
+            }
+          });
+      }
+    });
+  };
+
+
+
+
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     let filaE = null; // Variable para rastrear la fila en edición 
 
@@ -102,11 +251,13 @@ document.addEventListener("DOMContentLoaded", function () {
             text: "Email invalido...",
           });
     } else if( usuario == usuario.match(validarusuario) && nombre == nombre.match(validarnombre) && apellido == apellido.match(validarapellido) && telefono == telefono.match(validartelefono) && email == email.match(validaremail)){
+        registrar(usuario,nombre,apellido,telefono,email)
         Swal.fire({
             icon: "success",
             confirmButtonText: "Aceptar",
             text: "el usuario se agrego correctamente...",
           });
+
    
             // Si no hay fila en edición, crear una nueva fila en la tabla
             const nuevaF = document.createElement("tr");
@@ -129,6 +280,7 @@ document.addEventListener("DOMContentLoaded", function () {
             // Asociar evento de clic para editar usuarios a la nueva fila
             nuevaF.querySelector(".edit-button").addEventListener("click", function () {
                 const filaA = this.closest("tr");
+                updateCliente()
 
                 const usuario = filaA.cells[0].textContent;
                 const nombre = filaA.cells[1].textContent;
@@ -161,6 +313,7 @@ document.addEventListener("DOMContentLoaded", function () {
             // Asociar evento de clic para eliminar usuarios a la nueva fila
                 // Asociar evento de clic para eliminar usuarios a la nueva fila
                 nuevaF.querySelector(".delete-button").addEventListener("click", function () {
+                    eliminar()
                     Swal.fire({
                         icon: "question",
                         confirmButtonText: "Aceptar",
